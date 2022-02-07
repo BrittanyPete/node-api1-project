71485f3afbc9c868ce94ec90d990284c43b53d28
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-07 15:55:40 -0700

    installed dependencies, connected server and index.js, completed first get method

--- api/server.js
+++ api/server.js
@@ -1,3 +1,34 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Users = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+//ENDPOINTS:
+//GET /api/users - returns an array of users
+server.get('/api/users', (req, res) => {
+    //pull users from db
+    Users.find()
+    .then(users => {
+        //throw an error:
+        // throw new Error('foo error');
+        //send users back to client
+        res.json(users);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'could not get the users',
+            error: err.message
+        })
+    })
+    
+    
+})
+//GET /api/users/:id - returns the user object with the specified id.
+//POST /api/users - creates a user using the info sent inside the request body.
+//PUT /api/users/:id - Updates the user with the specified id using data from the request body. Returns the modified user.
+//DELETE /api/users/:id
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
