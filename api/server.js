1b26ae4f6244895fedac0b642cef5de80aae3114
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-07 16:46:05 -0700

    updated some of the messages to reflect readme instructions

--- api/server.js
+++ api/server.js
@@ -7,6 +7,26 @@ const server = express();
 server.use(express.json());
 
 //ENDPOINTS:
+
+//POST /api/users - creates a user using the info sent inside the request body.
+server.post('/api/users', async (req, res) => {
+    try {
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: 'Please provide name and bio for the user'
+            })
+        } else {
+            const newUser = await Users.insert(req.body)
+            res.status(201).json(newUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'There was an error while saving the user to the database',
+            error: err.message,
+        });
+    }
+});
+
 //GET /api/users - returns an array of users
 server.get('/api/users', (req, res) => {
     //pull users from db
@@ -19,7 +39,7 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
         res.status(500).json({
-            message: 'could not get the users',
+            message: 'The users information could not be retrieved',
             error: err.message
         });
     })
@@ -27,39 +47,48 @@ server.get('/api/users', (req, res) => {
 
 //GET /api/users/:id - returns the user object with the specified id.
 server.get('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
     try {
-        const user = await Users.find()
-        res.json(user)
+        const user = await Users.find(id)
+        if (!user) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist',
+            })
+        } else {
+            res.json(user)
+        }
     } catch (err) {
         res.status(500).json({
-            message: 'could not get the user',
+            message: 'The user information could not be retrieved',
             error: err.message,
         })
     }
 });
 
-//POST /api/users - creates a user using the info sent inside the request body.
-server.post('/api/users', async (req, res) => {
+
+//DELETE /api/users/:id
+
+
+//PUT /api/users/:id - Updates the user with the specified id using data from the request body. Returns the modified user.
+server.put('api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
     try {
-        if (!req.body.name || !req.body.bio) {
-            res.status(400).json({
-                message: 'name and bio are both required'
+        const updatedUser = await Users.update(id, body)
+        if (!updatedUser) {
+            res.status(404).json({
+                message: `user by id ${id} does not exist`
             })
         } else {
-            const newUser = await Users.insert(req.body)
-            res.status(201).json(newUser)
+            res.json(updatedUser);
         }
     } catch (err) {
         res.status(500).json({
-            message: 'error posting new user',
+            message: 'error editing user by id',
             error: err.message,
         });
     }
 });
-
-//PUT /api/users/:id - Updates the user with the specified id using data from the request body. Returns the modified user.
-
-//DELETE /api/users/:id
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
